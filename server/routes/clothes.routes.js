292e1ef055d95747df77ee82951259d23d36ecc1
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2022-05-09 22:18:41 +0700

    ok

--- server/routes/clothes.routes.js
+++ server/routes/clothes.routes.js
@@ -4,7 +4,7 @@ var router = express.Router()
 const controller = require('../controllers/clothes.controller')
 const { authenToken } = require('../middlewares/authenToken')
 
-router.get('/', authenToken, controller.getAll)
+router.get('/', controller.getAll)
 router.post('/', controller.create)
 router.delete('/deleteAll', controller.deleteAll)
 router.get('/:id', controller.getById)
--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -7,5 +7,8 @@ const { authenToken } = require('../middlewares/authenToken')
 router.post('/', controller.create)
 router.get('/', controller.getAll)
 router.get('/:id', controller.getById)
+router.delete('/deleteAll', controller.deleteAll)
+router.delete('/:id', controller.deleteOrderById)
+router.post('/:id', controller.updateOrderById)
 
 module.exports = router
