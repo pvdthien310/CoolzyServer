1f01bdbb546084e23c8ee6428a09b669b3a2cff3
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2022-06-12 00:50:53 +0700

    Update email.routes.js

--- server/routes/email.routes.js
+++ server/routes/email.routes.js
@@ -7,13 +7,14 @@ router.post('/verify', (req, res) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'coolzyproject@gmail.com',
-            pass: 'coolzyproject@1'
+            user: 'coolzyproject',
+            //pass: 'coolzyproject@1'
+            pass: 'hronxqtngsjupeba'
         }
     });
 
     var mailOptions = {
-        from: 'coolzyproject@gmail.com',
+        from: 'coolzyproject',
         to: req.body.to,
         subject: req.body.subject,
         text: req.body.text
